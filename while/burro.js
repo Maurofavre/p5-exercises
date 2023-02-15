018256262d3c7b32c5bc57b4ea5a31a85608c2d7
Mauro O. Favre <89173092+Maurofavre@users.noreply.github.com>
2023-02-15 00:21:21 -0300

    Update burro.js

--- while/burro.js
+++ while/burro.js
@@ -1,3 +1,9 @@
+
+// Burro-Mático
+// En este ejercicio deberás crear un programa que ayude al Burro a preguntar al usuario "¿Ya merito llegamos?".
+// Si el usuario responde “Si”, el programa deberá darle un mensaje de bienvenida a Muy Muy Lejano. Sino, deberá mostrarle, nuevamente, el mensaje del Burro.
+// ⚠️Importante: El programa deberá identificar como verdadero tanto “Si”, como “SI”, como “si”.
+
 let burro = prompt("Ya merito llegamos?");
 let result = burro.toLowerCase();
 
